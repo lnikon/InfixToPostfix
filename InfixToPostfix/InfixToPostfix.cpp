12d58474b7644577285c1983e289bfdfc53e09e4
Vahag Bejanyan <bejanyan@internal.synopsys.com>
2018-07-13 17:53:16 +0400

    Add project files.

---
+++ InfixToPostfix/InfixToPostfix.cpp
@@ -0,0 +1,12 @@
+﻿// InfixToPostfix.cpp : Defines the entry point for the application.
+//
+
+#include "InfixToPostfix.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
